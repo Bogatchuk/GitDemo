d3b520818246b0eecef7753d50ffec5db41c45b5
Bogatchuk Roman <bogatchukroma2013@gmail.com>
2021-11-26 10:23:55 +0200

    add gitignore file

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by MPro3.1 on 25.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello Hello hello")
+    }
+
+
+}
+
